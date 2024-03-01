70bb1448d92f950a9e3a82b2fd674399fddf98f5
xYujiax <scxie@ucsd.edu>
2024-02-29 18:39:40 -0800

    pray this works

---
+++ svelte/src/components/App.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<main>
+  <h1>Temperature Converter</h1>
+
+  <div id="converter">
+    <input type="text" id="celsius" placeholder="Celsius" />
+    <button id="submit" type="submit">Convert</button>
+  </div>
+
+  <div id="result">X degrees Celsius is Y degrees Fahrenheit</div>
+</main>
+
+<style>
+  main {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      'Helvetica Neue', Arial, sans-serif;
+    max-width: 800px;
+    margin: 5em auto;
+    padding: 1em;
+  }
+
+  #converter {
+    margin-bottom: 1em;
+  }
+</style>
