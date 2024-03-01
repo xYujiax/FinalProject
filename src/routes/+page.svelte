8757a38ef7ce72abdcc1abdd05baf8fb187816a8
ramosrenzo <j8ramos@ucsd.edu>
2024-02-29 19:24:55 -0800

    pls no merge error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,10 @@
   import App from '../components/App.svelte';
 </script>
 
-<App />
+<main>
+  <h1>Final Project Babbyyyyy</h1>
+  <div id="histogram"></div>
+  <App />
+  <p>something something CLT</p>
+</main>
+
