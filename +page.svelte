0d9c1e90040fb55902e15e4ba561219f882b747b
QuennieZeng <130181254+QuennieZeng@users.noreply.github.com>
2024-03-10 14:57:28 -0700

    new branchh

---
+++ +page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import App from '../components/App.svelte';
+</script>
+
+<main>
+  <h1>Final Project Prototype</h1>
+  <p>For our Final Project, we wanted to talk about the Central Limit Thereom. The Central Limit Theorem is a concept in statistics that describes the behavior of the sum or average of a 
+    large number of independent, identically distributed randome variables. The theoram states that, regardless of the shape of the original population distribution, the distribution of
+    the sample means will be approximately normally distributed if the sample size is sufficiently large. To show this, we are using the Iris dataset from the UCI machine learning repository
+    which has a shape of 150 rows and 4 columns. 
+  </p>
+  <App />
+  <p> We can see that the Central Limit Thereom is able to be applied to many types of datasets and at the end would create a curve similar to the bell curve.  
+  </p>
+  
+</main>
